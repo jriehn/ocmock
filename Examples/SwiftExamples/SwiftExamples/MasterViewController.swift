bff95dce67ea89f559894af91a3b1214180c1e2d
Jan Riehn <jan.riehn@web.de>
2014-12-15 19:50:38 +0100

    @jriehn upgraded swift syntax to XCode 6.1.1

--- Examples/SwiftExamples/SwiftExamples/MasterViewController.swift
+++ Examples/SwiftExamples/SwiftExamples/MasterViewController.swift
@@ -41,10 +41,7 @@ class MasterViewController: UITableViewController {
     }
 
     func insertNewObject(sender: AnyObject) {
-        if objects == nil {
-            objects = NSMutableArray()
-        }
-        objects.insertObject(NSDate.date(), atIndex: 0)
+        objects.insertObject(NSDate(), atIndex: 0)
         let indexPath = NSIndexPath(forRow: 0, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
     }
@@ -54,7 +51,7 @@ class MasterViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             let indexPath = self.tableView.indexPathForSelectedRow()
-            let object = objects[indexPath.row] as NSDate
+            let object = objects[indexPath!.row] as NSDate
             ((segue.destinationViewController as UINavigationController).topViewController as DetailViewController).detailItem = object
         }
     }
@@ -73,7 +70,7 @@ class MasterViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
         let object = objects[indexPath.row] as NSDate
-        cell.textLabel.text = object.description
+        cell.textLabel?.text = object.description
         return cell
     }
 
